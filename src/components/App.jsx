eff08e85c5cb15b762f5dded14f4f8d46120ac81
DexterDC <sebasdc9@hotmail.com>
2024-08-28 14:07:03 -0600

    created new component files

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen px-6 py-12 bg-gray-100 sm:px-12 sm:py-16">
+      <h1 className="text-3xl font-bold text-center text-gray-900">
+        Hello world!
+      </h1>
+      <p className="text-center text-gray-600">
+        This is the content of the app. You can add more content here.
+      </p>
+    </main>
+  );
+}
+
+export default App;
