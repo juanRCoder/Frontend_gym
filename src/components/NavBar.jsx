eff08d6e3bae495e94c03730b73552d73fb8c171
mpcevallos <mpcevallos@gmail.com>
2024-08-30 02:02:06 -0500

    Creando la carpeta img y subiendo imagenes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 function NavBar() {
-  return (
-    <nav className="border-b p-6">
-      <p className="text-4xl font-bold">This is the nav section</p>
+  return (      
+    <nav className="flex w-full h-[131px] bg-header">
+   <img src="./src/assets/img/powerzone_logo.png" className="m-10 ml-20 w-[296px] h-[57px] align-items-center justify-content-center " alt="Logo PowerZone"/>
     </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        header: "var(--header)",
+        dark: "var(--dark)",
+        lightGray: "var(--light-gray)",
+        darkMid: "var(--dark-mid)",
+        grayMid: "var(--gray-mid)",
+        gray: "var(--gray)",
+        white: "var(--white)",
+        yellow: "var(--yellow)",
+      },
+      backgroundColor: {
+        header: "var(--header)",
+        yellow: "var(--yellow)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
