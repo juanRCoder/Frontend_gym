36628d69cf72dc516fbdd08b6794914b0755e447
María Paula Cevallos <132369182+mpcevallos@users.noreply.github.com>
2024-09-02 18:27:52 -0500

    Revert "Feature styles icons (#2)"
    This reverts commit 13b37255d50a3f1119cf0ee9c6f4fa73ea58018e.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="border-b p-6 bg-yellow">
-      <img className="m-10 w-[296px] h-[57px]" src="./src/assets/img/powerzone_logo_gray.png" alt="PowerZone Gray Logo"/>
-      <div><p className="text-1xl font-bold text-black">¡Únete a nosotros y sé parte de esta
-historia de superación y éxito!</p></div>
+    <footer className="border-b p-6">
+      <p className="text-4xl font-bold">This is the footer</p>
     </footer>
   );
 }
