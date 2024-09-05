fe2e14b99accbc19b0174a420181c3a6945864d7
danielbenitez <daniel29052003@gmail.com>
2024-09-04 23:30:41 -0300

    navegacion

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-
+import Questions from "../components/questions/Questions";
 function Layout() {
   return (
     <>
-      <NavBar />
-      <Outlet />
-      <Footer />
+    <NavBar />
+
+     <Questions/>
+     <Footer/>
     </>
   );
 }
