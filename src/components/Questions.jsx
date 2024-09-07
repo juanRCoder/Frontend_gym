eabad46b12a9e35f12558c715debfe39c3cdd9df
danielbenitez <daniel29052003@gmail.com>
2024-09-06 23:11:56 -0300

    style: cambio de tamaño al contenedor padre a 59rem

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -30,14 +30,14 @@ function Questions() {
     }
   }
   return (
-    <article className='min-h-screen flex justify-center bg-black '>
-      <section className='w-11/12'>
+    <article className='min-h-screen flex mx-auto justify-center bg-black '>
+      <section className='w-[59rem] mx-auto'>
 
         <section className='ml-4 mt-12'>
           <div className='w-20 h-0.5 bg-[color:var(--yellow)]'></div>
           <h3 className='text-4xl font-bold mt-3 text-white'>PREGUNTAS FRECUENTES</h3>
         </section>
-        <section className='mt-8 ml-4 h-4/5 flex flex-col gap-3 ' >
+        <section className='mt-8 ml-4 h-4/5 flex flex-col  gap-3 ' >
           <section  id='faqs-1' onClick={(e) => handleDown(e)} className='cursor-pointer rounded-md w-11/12  flex flex-col items-center justify-between overflow-y-hidden bg-dark h-11' >
             <div className='flex items-center justify-between w-full p-1 '> <h3 className='text-[color:var(--yellow)] text-3xl font-bold ml-4 oswald-medium'>¿listo para transformar tu vida? </h3> {IsFaqsOne ? <HiChevronRight fontSize={30} className='text-[color:var(--yellow)] pt-1'/> : <HiChevronDown className='text-[color:var(--yellow)]' fontSize={30}  />}  </div>
             <div className='bg-[#222932] rounded-b-lg mt-2 h-60'> <p className='p-3 z-10 text-2xl text-[color:var(--white)]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum reiciendis, explicabo ad molestiae est sequi dolore aliquam voluptatibus nisi expedita id officia esse adipisci, aspernatur quia! Nobis maiores officiis consectetur!</p> </div>
