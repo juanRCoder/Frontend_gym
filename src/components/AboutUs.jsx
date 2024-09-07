0ac89bbc5d4b84857d2827c189f5cb69b884549a
María Paula Cevallos <132369182+mpcevallos@users.noreply.github.com>
2024-09-07 14:59:37 -0500

    refactor: cambiando el ancho de los componentes por w-[59rem] en lugar de max-w-[59rem] (#14)

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
 import Trainer from "../assets/img/trainer_home.png";
-import Services from './Services';
+import Services from "./Services";
 
 function AboutUs() {
   return (
     <>
-      <Services className="w-max-[59rem] mx-auto" />
-      <div className='flex flex-col w-max-[937px] mx-auto lg:flex-row bg-black justify-center gap-x-[10rem] lg:space-x-[2rem] space-y-8 lg:space-y-0 p-8 lg:p-8'>
-        <div className='text-white w-full lg:w-[30rem] mt-[2rem] lg:mt-[3rem] mb-[2rem] lg:mb-[5rem] text-base lg:text-lg'>
-          <p className='text-lg lg:text-xl' style={{ lineHeight: "2.5rem" }}>
-            Somos el resultado de un sueño que comenzó con una simple idea: transformar vidas a través del poder del fitness. Lo que empezó como un pequeño espacio para entrenar se ha convertido en un lugar donde la comunidad se reúne, las metas se alcanzan y la pasión por un estilo de vida saludable nos impulsa cada día. <br />Aquí, cada logro, grande o pequeño, es celebrado.
+      <Services className="md:w-[59rem] mx-auto" />
+      <div className="flex flex-col md:w-[59rem] mx-auto lg:flex-row bg-black justify-center gap-x-[10rem] lg:space-x-[2rem] space-y-8 lg:space-y-0 p-8 lg:p-8">
+        <div className="text-white w-full lg:w-[30rem] mt-[2rem] lg:mt-[3rem] mb-[2rem] lg:mb-[5rem] text-base lg:text-lg">
+          <p className="text-lg lg:text-xl" style={{ lineHeight: "2.5rem" }}>
+            Somos el resultado de un sueño que comenzó con una simple idea:
+            transformar vidas a través del poder del fitness. Lo que empezó como
+            un pequeño espacio para entrenar se ha convertido en un lugar donde
+            la comunidad se reúne, las metas se alcanzan y la pasión por un
+            estilo de vida saludable nos impulsa cada día. <br />
+            Aquí, cada logro, grande o pequeño, es celebrado.
           </p>
           <br />
-          <p className='text-lg lg:text-xl' style={{ lineHeight: "2.5rem" }}>
-            En nuestro gimnasio, no solo entrenamos el cuerpo; fortalecemos la mente y el espíritu, creando una comunidad unida por la dedicación y el esfuerzo.
+          <p className="text-lg lg:text-xl" style={{ lineHeight: "2.5rem" }}>
+            En nuestro gimnasio, no solo entrenamos el cuerpo; fortalecemos la
+            mente y el espíritu, creando una comunidad unida por la dedicación y
+            el esfuerzo.
           </p>
         </div>
         <img
