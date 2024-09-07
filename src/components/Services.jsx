375d0dea39abc95e0aacc84cec6691f8a994ff4b
mpcevallos <mpcevallos@gmail.com>
2024-09-06 23:08:44 -0500

    styles: Modificando colores y agregando transicion a íconos de servicios

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,28 +8,28 @@ function Services() {
   return (
     <>
     <div className="bg-dark py-[4rem] w-max-[59rem]">
-      <section className="container mx-auto flex flex-col lg:flex-row justify-between space-y-[2rem] lg:space-y-0 px-4 lg:px-8 text-yellow">
+      <section className="container mx-auto flex flex-col lg:flex-row justify-between space-y-[2rem] lg:space-y-0 px-4 lg:px-8 text-white">
         <section className="flex flex-col items-center">
           <h2 className="text-base lg:text-lg text-center">
             TU ELIGES TU HORARIO
           </h2>
-          <HiOutlineCalendar className="text-5xl lg:text-6xl text-white mt-[1rem] transition duration-300 hover:scale-110 hover:text-yellow" />
+          <HiOutlineCalendar className="text-5xl lg:text-6xl text-yellow mt-[1rem] transition duration-300 hover:scale-[125%] hover:rotate-[0.5deg] hover:text-yellow" />
         </section>
         <section className="flex flex-col items-center">
           <h2 className="text-base lg:text-lg text-center">
             PLANES PERSONALIZADOS
           </h2>
-          <BsFire className="text-5xl lg:text-6xl text-white mt-[1rem] transition duration-300 hover:scale-110 hover:text-yellow" />
+          <BsFire className="text-5xl lg:text-6xl text-yellow mt-[1rem] transition duration-300 hover:scale-[125%] hover:rotate-[0.5deg] hover:text-yellow" />
         </section>
         <section className="flex flex-col items-center">
           <h2 className="text-base lg:text-lg text-center">EQUIPOS MODERNOS</h2>
-          <IoIosFitness className="text-5xl lg:text-6xl text-white mt-[1rem] transition duration-300 hover:scale-110 hover:text-yellow" />
+          <IoIosFitness className="text-5xl lg:text-6xl text-yellow mt-[1rem] transition duration-300 hover:scale-[125%] hover:rotate-[0.5deg] hover:text-yellow" />
         </section>
         <div className="flex flex-col items-center">
           <h2 className="text-base lg:text-lg text-center">
             TRAINERS PROFESIONALES
           </h2>
-          <BsFillPeopleFill className="text-5xl lg:text-6xl text-white mt-[1rem] transition duration-300 hover:scale-110 hover:text-yellow" />
+          <BsFillPeopleFill className="text-5xl lg:text-6xl text-yellow mt-[1rem] transition duration-300 hover:scale-[125%] hover:rotate-[0.5deg] hover:text-yellow" />
         </div>
     </section>
     </div>
