f56ded19ccd709bf43727ed5942a3d9bf2c34953
John <johnelvis963@gmail.com>
2024-09-09 13:50:46 -0500

    fix: Mejora en el codigo del carousel responsi

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <VideoSection />
       <AboutUs />
-      <TestimonialUsers className={'h-[26rem] w-[75rem]'} />
+      <TestimonialUsers className={'md:h-[26rem] max-w-[75rem]'} />
     </>
   );
 }
