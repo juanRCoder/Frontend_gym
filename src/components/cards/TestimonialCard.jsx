fd043af930873d9ff6519f74fc525f4acba25112
John <johnelvis963@gmail.com>
2024-09-13 15:18:54 -0500

    fix: el archivo TestimonialCard no contenia un componente xd

--- src/components/cards/TestimonialCard.jsx
+++ src/components/cards/TestimonialCard.jsx
@@ -1,37 +1,21 @@
+
 import { string } from 'prop-types';
-import { CustomSlider } from './slider/CustomSlider';
-import { TestimonialCard } from './cards/TestimonialCard';
-import { dataTestimonios } from '../data/dataTestimonios';
 
-export const TestimonialUsers = ({ url_image, className }) => {
+export const TestimonialCard = ({ testimony, name }) => {
+
    return (
-      <section className={`text-white w-full relative`}>
-         <img
-            className="absolute w-full h-full object-cover object-top"
-            src={`${url_image || './src/assets/img/testimonials_bg.png'}`}
-            alt="img de fondo testimonial"
-         />
-         <div className={`mx-auto relative p-5 md:pt-10 space-y-4 flex  flex-col md:space-y-8   ${className}`}>
-            <div className='relative w-fit md:text-3xl tracking-wide font-semibold space-y-2 uppercase'>
-               <div className='border-t-2 border-yellow w-[70%] rounded-md'></div>
-               <p>
-                  testimonios <br />
-                  de éxito
-               </p>
-            </div>
-            {
-               <CustomSlider
-                  data={dataTestimonios}
-                  ChildComponent={TestimonialCard}
-               />
-            }
-         </div>
-      </section>
-   );
-};
+      <div className="mt-5 mb-8 text-sm md:text-xl w-full text-center space-y-4 uppercase md:mb-0">
+         <h3 className=''>
+            {testimony}
+         </h3>
+         <p className="w-[10rem] md:w-[13rem] mx-auto font-bold text-dark rounded-3xl bg-yellow p-2 place-self-center">
+            - {name}
+         </p>
+      </div>
+   )
+}
 
-// PropTypes de react type
-TestimonialUsers.propTypes = {
-   url_image: string,
-   className: string,
+TestimonialCard.propTypes = {
+   testimony: string,
+   name: string,
 };
