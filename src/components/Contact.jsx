959f12284c1914b69f8176c62671274b61f33bca
Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
2024-09-13 19:53:59 -0500

    Feature contact (responsive) (#21)
    * refactor: implementar section contact
    
    * refactor: ajustar anchos y vista general
    
    * feat: implementar la seccion de contacto
    
    * fix: corregir etiquetas repetidas
    
    * fix: cambiar el tamaño de titulo
    
    * feat: implementacion para adaptar a diferentes dispositivos
    
    ---------
    
    Co-authored-by: John Elvis <johnelvis963@gmail.com>

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,12 +4,12 @@ import InfoContact from "./form/InfoContact";
 function Contact() {
   return (
     <aside className="bg-black py-10">
-      <section className="max-w-6xl m-auto">
+      <section className="max-w-6xl m-auto p-5 lg:p-0">
         <header className="relative w-fit md:text-3xl tracking-wide space-y-2">
           <div className="border-t-[3px] border-yellow w-[65%] pb-2"></div>
           <p className="text-white font-normal">CONTÁCTANOS</p>
         </header>
-        <div className="flex gap-10 pb-10">
+        <div className="flex flex-col sm:flex-row gap-10">
           <FormContact />
           <InfoContact />
         </div>
