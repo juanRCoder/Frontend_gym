148fc6eae14b9c4f55a43dead372165431dfdd43
danielbenitez <daniel29052003@gmail.com>
2024-09-19 23:52:42 -0300

    component actualizado con Shadcn

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import Accordeon from "../components/Accordeon";
+
 function Layout() {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Accordeon/>
       <Footer />
     </>
   );
