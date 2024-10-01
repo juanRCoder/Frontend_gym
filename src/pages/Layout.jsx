82dc717ba3bce2974150760acd0f32ebab4aef2d
Conitoweb <98422682+Conitoweb@users.noreply.github.com>
2024-10-01 14:57:46 -0300

    Revert "Feature faqs"

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import Accordeon from "../components/Accordeon";
 
 function Layout() {
   return (
     <>
       <NavBar />
       <Outlet />
-      <Accordeon/>
       <Footer />
     </>
   );
