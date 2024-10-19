26fdef44e3bffb71ffa8b0c5c3a959d420ac28d6
Juan Ramirez <124215732+juanRCoder@users.noreply.github.com>
2024-10-18 20:59:59 -0500

    Branch: Feature update landing page (#26)
    * refactor: implementar section contact
    
    * refactor: ajustar anchos y vista general
    
    * feat: implementar la seccion de contacto
    
    * fix: corregir etiquetas repetidas
    
    * fix: cambiar el tamaño de titulo
    
    * feat: implementacion para adaptar a diferentes dispositivos
    
    * refactor: componetizar el navbar
    
    * feat: agregar nuevos cambios
    
    * styles: actualizacion final navbar y hero
    
    * refactor: refactorizar codigo y estilos

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,36 +1,35 @@
-
-import Trainer from "../assets/img/trainner_img.png";
-import Services from "./Services";
+import Trainer from "../assets/img/TRAINNER_IMG.png";
 
 function AboutUs() {
   return (
     <>
-      <Services/>
       <article className="bg-black">
-        <section className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-center items-center lg:gap-32 py-28 w-full lg:w-[1085px]">
-          <div className="text-white w-fit lg:w-[534px] text-justify select-none">
-            <p className="text-lg lg:text-2xl mb-5" style={{lineHeight: "3.2rem"}}>
-              Somos el resultado de un sueño que comenzó con una simple idea:
-              transformar vidas a través del poder del fitness. Lo que empezó
-              como un pequeño espacio para entrenar se ha convertido en un lugar
-              donde la comunidad se reúne, las metas se alcanzan y la pasión por
-              un estilo de vida saludable nos impulsa cada día. <br />
-              Aquí, cada logro, grande o pequeño, es celebrado.
-            </p>
-            <p className="text-lg lg:text-2xl" style={{lineHeight: "3.2rem"}}>
-              En nuestro gimnasio, no solo entrenamos el cuerpo; fortalecemos la
-              mente y el espíritu, creando una comunidad unida por la dedicación
-              y el esfuerzo.
-            </p>
-          </div>
-        
-          <img
-            className="w-[477px] h-[525px]"
-            src={Trainer}
-            alt="Trainer"
-          />
-        
-        </section>
+        <div className="max-w-7xl m-auto">
+          <section className="flex flex-col lg:flex-row items-center justify-between mx-8 lg:mx-24 py-24 gap-10 sm:gap-10">
+            <div className="lg:max-w-[509px] text-[22px] text-white leading-[44px]">
+              <p>
+                Somos el resultado de un sueño que comenzó con una simple idea:
+                transformar vidas a través del poder del fitness. Lo que empezó
+                como un pequeño espacio para entrenar se ha convertido en un
+                lugar donde la comunidad se reúne, las metas se alcanzan y la
+                pasión por un estilo de vida saludable nos impulsa cada día.
+                Aquí, cada logro, grande o pequeño, es celebrado.
+              </p>
+              <p>
+                En nuestro gimnasio, no solo entrenamos el cuerpo; fortalecemos
+                la mente y el espíritu, creando una comunidad unida por la
+                dedicación y el esfuerzo.
+              </p>
+            </div>
+            <div>
+              <img
+                className="object-contain  sm:max-h-[509px] sm:max-w-[463px]"
+                src={Trainer}
+                alt="Trainer"
+              />
+            </div>
+          </section>
+        </div>
       </article>
     </>
   );
--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -2,33 +2,39 @@ import RAY_DARK from "../assets/img/ray_day.png";
 
 function VideoSection() {
   return (
-    <section className="h-[650px] relative top-0">
-      <video autoPlay loop muted className="w-full h-full object-cover">
+    <section className="h-[742px] relative ">
+      <video
+        autoPlay
+        loop
+        muted
+        className="w-full h-full object-cover absolute"
+      >
         <source
           src="https://res.cloudinary.com/dgfkry5fw/video/upload/v1725319178/6388397-uhd_3840_2160_25fps_gogmcc.mp4"
           type="video/mp4"
         />
       </video>
-      <section className="lg:w-[1085px] absolute left-1/2 -translate-x-1/2 bottom-0 w-full max-w-7xl mx-auto flex justify-center sm:justify-start">
 
-        <div className="ml-7 2xl:ml-0 relative h-16 bottom-0 mb-[170px] flex flex-col gap-4 text-black">
-          <hr className="w-[55%] border-[2px] border-yellow rounded-full" />
-          <h1 className="sm:text-left text-left text-4xl sm:text-5xl text-white mb-2">
-            HARD ROCK GAME.<br />TIME TO CHANGE
-          </h1>
-          <div className="mx-auto sm:m-0 w-full flex items-center justify-center">
-            <button className="flex gap-3 items-center justify-center text-xl sm:text-2xl bg-yellow rounded-full py-2 font-medium hover:bg-slate-200 transition min-w-full">
-              CONOCE MÁS
-              <img 
-                src={RAY_DARK}
-                width={20}
-                height={20}
-              />
-            </button>
-          </div>
+      <div className="max-w-7xl mx-auto flex items-end justify-end h-full">
+        <div className="w-full relative">
+          <section className="absolute bottom-16 left-1/2 sm:left-9 lg:left-24 transform -translate-x-1/2 sm:transform-none">
+            <div className="relative flex flex-col gap-4 text-black">
+              <hr className="w-[55%] border-[2px] border-yellow rounded-full" />
+              <h1 className="w-64 sm:w-auto  sm:text-left text-left text-4xl lg:text-5xl text-white mb-2">
+                HARD ROCK GAME.
+                <br />
+                TIME TO CHANGE
+              </h1>
+              <div className="mx-auto sm:m-0 w-full flex items-center justify-center">
+                <button className="flex gap-3 items-center justify-center text-xl sm:text-2xl bg-yellow rounded-full py-2 font-medium hover:bg-slate-200 transition min-w-full">
+                  CONOCE MÁS
+                  <img src={RAY_DARK} width={20} height={20} />
+                </button>
+              </div>
+            </div>
+          </section>
         </div>
-
-      </section>
+      </div>
     </section>
   );
 }
