d829b78152d5a07425683abe967ec34f4b8b2be2
John <johnelvis963@gmail.com>
2024-10-20 21:05:59 -0500

    feat: Componente reutilizable para implementar en paginas

--- src/pages/locales/components/texts/TitleCustom.jsx
+++ src/pages/locales/components/texts/TitleCustom.jsx
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 
 export const TitleCustom = ({ textPrimary, textSecondary }) => {
    return (
-      <p className='w-fit mx-auto text-center uppercase font-medium text-3xl md:text-5xl'>
+      <h1 className='w-fit mx-auto text-center uppercase font-medium text-3xl md:text-5xl'>
          <span className={`block`}>{textPrimary}</span>
          <span className={`text-[#f4af00]`}>{textSecondary}</span>
-      </p>
+      </h1>
    );
 };
 
