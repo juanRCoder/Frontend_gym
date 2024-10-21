407c83f64fd6fba1be136936b8e173f3eabc488b
John <johnelvis963@gmail.com>
2024-10-20 20:39:35 -0500

    fix: Agregada la ruta locales. reemplace el anchor a un componente  Link de react-router-dom

--- src/components/navbar/DropdownMenu.jsx
+++ src/components/navbar/DropdownMenu.jsx
@@ -1,18 +1,20 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function DropdownMenu({ isDropdownOpen }) {
   return (
     <div
-      className={`absolute left-0 top-[3.35rem] w-full bg-slate-200 text-black shadow-lg transform transition-opacity duration-300 ease-out select-none ${
-        isDropdownOpen ? "opacity-80" : "opacity-0"
-      } z-50`}
+      className={`absolute left-0 top-[3.35rem] w-full bg-slate-200 text-black shadow-lg transform transition-opacity duration-300 ease-out select-none ${isDropdownOpen ? "opacity-80" : "opacity-0"
+        } z-20`}
     >
       <ul>
-        {[["Locales"], ["Trainers"], ["Planes"]].map(([option], i) => (
+        {[["locales"], ["Trainers"], ["Planes"]].map(([option], i) => (
           <li key={i}>
-            <a className="text-lg block px-4 py-2 hover:bg-gray-200 hover:bg-yellow hover:text-white cursor-pointer">
-              {option}
-            </a>
+            <Link to={option}>
+              <span className="text-lg block px-4 py-2 hover:bg-gray-200 hover:bg-yellow hover:text-white cursor-pointer capitalize">
+                {option}
+              </span>
+            </Link>
           </li>
         ))}
       </ul>
