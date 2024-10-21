07655b1186e4e2a20f77dd9dfd1b8eef4d34bf6f
John <johnelvis963@gmail.com>
2024-10-21 09:31:43 -0500

    md:leading-[3.5rem]

--- src/pages/locales/components/texts/TitleCustom.jsx
+++ src/pages/locales/components/texts/TitleCustom.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export const TitleCustom = ({ textPrimary, textSecondary }) => {
    return (
-      <h1 className='w-fit mx-auto text-center uppercase font-medium text-3xl md:text-5xl'>
+      <h1 className='w-fit mx-auto text-center uppercase font-medium text-3xl md:text-5xl md:leading-[3.5rem]'>
          <span className={`block`}>{textPrimary}</span>
          <span className={`text-[#f4af00]`}>{textSecondary}</span>
       </h1>
