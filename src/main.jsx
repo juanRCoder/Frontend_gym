3b8a3f64cf24d73cd25fd5c04c6e33952ac11180
John <johnelvis963@gmail.com>
2024-10-23 12:36:33 -0500

    feat: Agregado la ruta /locales

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./pages/Layout";
 import PageLocales from "./pages/locales/PageLocales";
 import './index.css'
+import './animation.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
@@ -24,8 +26,11 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
