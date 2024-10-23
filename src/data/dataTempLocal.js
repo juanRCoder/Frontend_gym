791fa3ccd6000c737a011b1a527dbd8fb5d83802
John <johnelvis963@gmail.com>
2024-10-23 13:03:29 -0500

    data temporal

---
+++ src/data/dataTempLocal.js
@@ -0,0 +1,19 @@
+export const services = [
+   {
+      "id": 1,
+      "name": "Personal trainer",
+      "icon": "https://res.cloudinary.com/...",
+      "created_at": "2024-01-01T12:00:00Z",
+      "updated_at": "2024-01-01T12:00:00Z"
+   },
+]
+export const clases = [
+   {
+      "id": 1,
+      "name": "Baile",
+      "icon": "https://res.cloudinary.com/...",
+      "created_at": "2024-01-01T12:00:00Z",
+      "updated_at": "2024-01-01T12:00:00Z"
+   },
+]
+
