0893d7dd3fd796e6c9d1c5ccae3c94209f0771d9
John <johnelvis963@gmail.com>
2024-10-23 13:44:40 -0500

    feat: Barril  de importaciones

---
+++ src/pages/locales/components/index.js
@@ -0,0 +1,6 @@
+export * from './card/CardLocal'; 
+export * from './card/Locals'; 
+export * from './filtro/FiltroLocal'; 
+export * from './input/InputSearch'; 
+export * from '@/components/texts/TitleCustom'; 
+export * from '@/components/boton/ButtomCuston'; 
