9047ffe6d03cb8a695a0fe849dbc135657e7ecec
John <johnelvis963@gmail.com>
2024-10-23 12:22:01 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'), // Esto establece el alias @ para src
+      '@': resolve(__dirname, 'src'),
     },
   },
 });
