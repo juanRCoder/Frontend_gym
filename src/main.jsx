69a2382dc3f93e3009d642b862f3d472fdf20641
John <johnelvis963@gmail.com>
2024-10-23 14:07:23 -0500

    feat:

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./pages/Layout";
-import PageLocales from "./pages/locales/PageLocales";
-import './index.css'
-import './animation.css'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Landing from "./pages/Landing";
+import LayoutLanding from "./layout/LayoutLanding";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: <LayoutLanding />,
     children: [
       {
         path: "/",
-        element: <App />,
+        element: <Landing />,
       },
-      {
-        path: "/locales",
-        element: <PageLocales />,
-      }
     ],
   },
 ]);
 
-const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
